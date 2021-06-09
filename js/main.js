ae16b29e35b3a358cf9692f41e309f996f3bd342
3ndymi0n <endymi0n@systemdecay.info>
2021-06-10 07:00:47 +1000

    Added fluff that will be deleted later.

--- js/main.js
+++ js/main.js
@@ -1,51 +1,51 @@
-const MOVES = ['scissors', 'paper', 'rock'];
+const MOVES = ["scissors", "paper", "rock"];
 const RULES = {
-    'paper': 'rock',
-    'scissors': 'paper',
-    'rock':'scissors'
-}
+  paper: "rock",
+  scissors: "paper",
+  rock: "scissors",
+};
 
 function computerPlay() {
-    return MOVES[Math.floor((Math.random()*MOVES.length))];
+  return MOVES[Math.floor(Math.random() * MOVES.length)];
 }
 
-
 function playerPlay() {
-    let play = prompt("scissors, paper or rock?");
-    return play;
+  let play = prompt("scissors, paper or rock?");
+  return play;
 }
 
 function gameRound(playerTurn, computerTurn) {
-    if(RULES[playerTurn] == computerTurn) {
-        return "player";
-    }else if(RULES[computerTurn] == playerTurn) {
-        return "computer";
-    }else {
-        return "draw";
-    }
+    // Play one iteration of the game.
+  if (RULES[playerTurn] == computerTurn) {
+    return "player";
+  } else if (RULES[computerTurn] == playerTurn) {
+    return "computer";
+  } else {
+    return "draw";
+  }
 }
 
 function game() {
-    let playerScore = 0;
-    let computerScore = 0;
+    // play 5 rounds of the game, highest score wins.
+  let playerScore = 0;
+  let computerScore = 0;
 
-
-    for(let i = 0; i < 5; i++) {
-        let round = gameRound(playerPlay(), computerPlay());
-        if(round == "draw") {
-            console.log("Draw");
-        }else if(round == 'player') {
-            console.log("Player wins round!")
-            playerScore++;
-        }else if(round == 'computer') {
-            console.log("Computer wins round!")
-            computerScore++
-        }
+  for (let i = 0; i < 5; i++) {
+    let round = gameRound(playerPlay(), computerPlay());
+    if (round == "draw") {
+      console.log("Draw");
+    } else if (round == "player") {
+      console.log("Player wins round!");
+      playerScore++;
+    } else if (round == "computer") {
+      console.log("Computer wins round!");
+      computerScore++;
     }
+  }
 
-    if(playerScore > computerScore) {
-        alert("Player Wins! " + playerScore + "/" + computerScore)
-    }else {
-        alert("Computer Wins! " + computerScore + "/" + playerScore)
-    }
+  if (playerScore > computerScore) {
+    alert("Player Wins! " + playerScore + "/" + computerScore);
+  } else {
+    alert("Computer Wins! " + computerScore + "/" + playerScore);
+  }
 }
